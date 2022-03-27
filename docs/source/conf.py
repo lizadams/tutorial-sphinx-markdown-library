79e0684108f338b412f19daaceb1ed7dcd66b3b1
lizadams <lizadams@email.unc.edu>
2022-03-27 15:32:09 -0400

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,7 @@ release = '0.1'
 extensions = ["myst_parser",
               "sphinx.ext.duration",
               "sphinx.ext.autosectionlabel",
+              "sphinx.ext.autodoc",
               "nbsphinx", #MyST-NB
 ]
 
